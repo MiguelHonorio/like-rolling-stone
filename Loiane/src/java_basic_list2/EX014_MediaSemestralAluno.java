e28396dd4f2f2ff3ea4c3a96c2aba6c9cba8fc9e
Miguel Honorio <miguelshs@gmail.com>
2018-05-07 17:00:50 -0300

    Novos exercícios

---
+++ Loiane/src/java_basic_list2/EX014_MediaSemestralAluno.java
@@ -0,0 +1,47 @@
+package java_basic_list2;
+
+import java.util.Scanner;
+
+public class EX014_MediaSemestralAluno {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Entre com a primeira nota:");
+		double nota1 = scan.nextDouble();
+		System.out.println("Entre com a segunda nota:");
+		double nota2 = scan.nextDouble();
+		
+		double media = (nota1 + nota2)/2;
+		
+		String aproveitamento = "";
+		if(media >= 9.0 && media <= 10.0) {
+			aproveitamento = "A";
+		}else if(media >= 7.5 && media < 9.0) {
+			aproveitamento = "B";
+		}else if(media >= 6.0 && media < 7.5) {
+			aproveitamento = "C";
+		}else if(media >= 4.0 && media < 6.0) {
+			aproveitamento = "D";
+		}if(media >= 0.0 && media < 4.0) {
+			aproveitamento = "E";
+		}
+
+		System.out.println("Nota1: " +nota1);
+		System.out.println("Nota1: " +nota2);
+		System.out.println("Média: " +media);
+		System.out.println("Conceito:" +aproveitamento);
+		
+		switch(aproveitamento) {
+		case "A":
+		case "B":
+		case "C":
+			System.out.println("Aprovado");break;
+		case "D":
+		case "E":
+			System.out.println("Reprovado");
+		}
+	}
+
+}
---
+++ Loiane/src/java_basic_list2/EX015_Triangulo.java
@@ -0,0 +1,21 @@
+package java_basic_list2;
+
+import java.util.Scanner;
+
+public class EX015_Triangulo {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Entre com a 1° medida: ");
+		double medida1 = scan.nextDouble();
+		System.out.println("Entre com a 2° medida: ");
+		double medida2 = scan.nextDouble();
+		System.out.println("Entre com a 3° medida: ");
+		double medida3 = scan.nextDouble();
+		
+		
+		
+	}
+
+}
