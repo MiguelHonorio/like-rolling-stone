c6341ab6305c1da8ea388221591aeb670f9b1902
Miguel Honorio <miguelshs@gmail.com>
2018-05-08 16:55:06 -0300

    Novos exercícios

--- Loiane/src/java_basic_list2/EX015_Triangulo.java
+++ Loiane/src/java_basic_list2/EX015_Triangulo.java
@@ -13,9 +13,18 @@ public class EX015_Triangulo {
 		double medida2 = scan.nextDouble();
 		System.out.println("Entre com a 3° medida: ");
 		double medida3 = scan.nextDouble();
-		
-		
-		
-	}
 
+		if (medida1 + medida2 < medida3 || medida1 + medida3 < medida2 || medida2 + medida3 < medida1) {
+			System.out.println("Não é um triângulo");
+		} else {
+			if (medida1 == medida2 && medida2 == medida3 && medida3 == medida1) {
+				System.out.println("É um triângulo equilátero");
+			} else if (medida1 == medida2 || medida2 == medida3 || medida3 == medida1) {
+				System.out.println("É um triângulo Isóceles");
+			} else if (medida1 != medida2 && medida2 != medida3 && medida3 != medida1) {
+				System.out.println("É um triângulo Escaleno");
+
+			}
+		}
+	}
 }
